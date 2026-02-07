4351ebdbed2ac5c9baf27322694bb7143b343eb9
Ismail <ismailtester@yandex.ru>
2026-02-07 22:02:28 +0300

    Практика написания автотестов

---
+++ test_pytest.py
@@ -0,0 +1,12 @@
+def test_user_login():
+    print("Hello world")
+
+
+class TestUserLogin:
+    def test_1(self):
+        a = 4
+        b = 5
+        assert a == b, f"{a} != {b}"
+
+    def test_2(self):
+        pass
