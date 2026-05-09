2db9ebd6e064ad4d027453596d567f96ba0e3863
Ismail <ismailtester@yandex.ru>
2026-05-09 21:43:37 +0300

    Практикуемся в реализации PageComponent

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -1,6 +1,10 @@
 from playwright.sync_api import expect, Page
+
+from components.courses.course_view_component import CourseViewComponent
+from components.courses.courses_list_toolbar_view_component import CoursesListToolbarViewComponent
 from components.navigation.sidebar_component import SidebarComponent
 from components.navigation.navbar_component import NavbarComponent
+from components.views.empty_view_component import EmptyViewComponent
 
 from pages.base_page import BasePage
 
@@ -11,77 +15,16 @@ class CoursesListPage(BasePage):
 
         self.sidebar = SidebarComponent(page)
         self.navbar = NavbarComponent(page)
+        self.empty_view = EmptyViewComponent(page, identifier="courses-list")
+        self.course_view = CourseViewComponent(page)
+        self.toolbar_view = CoursesListToolbarViewComponent(page)
 
-
-        # Заголовок и кнопка создания курса
-        self.courses_title = self.page.get_by_test_id('courses-list-toolbar-title-text')
-        self.create_course_button = self.page.get_by_test_id('courses-list-toolbar-create-course-button')
-
-        # Пустой блок при отсутствии курсов
-        self.empty_view_icon = self.page.get_by_test_id('courses-list-empty-view-icon')
-        self.empty_view_title = self.page.get_by_test_id('courses-list-empty-view-title-text')
-        self.empty_view_description = self.page.get_by_test_id('courses-list-empty-view-description-text')
-
-        # Карточка курса
-        self.course_title = self.page.get_by_test_id('course-widget-title-text')
-        self.course_image = self.page.get_by_test_id('course-preview-image')
-        self.course_max_score_text = self.page.get_by_test_id('course-max-score-info-row-view-text')
-        self.course_min_score_text = self.page.get_by_test_id('course-min-score-info-row-view-text')
-        self.course_estimated_time_text = self.page.get_by_test_id('course-estimated-time-info-row-view-text')
-
-        # Меню курса
-        self.course_menu_button = self.page.get_by_test_id('course-view-menu-button')
-        self.course_edit_menu_item = self.page.get_by_test_id('course-view-edit-menu-item-text')
-        self.course_delete_menu_item = self.page.get_by_test_id('course-view-delete-menu-item-text')
-
-
-    def check_visible_courses_title(self):
-        expect(self.courses_title).to_be_visible()
-        expect(self.courses_title).to_have_text("Courses")
 
     def check_visible_empty_view(self):
-        expect(self.empty_view_icon).to_be_visible()
-
-        expect(self.empty_view_title).to_be_visible()
-        expect(self.empty_view_title).to_have_text("There is no results")
-
-        expect(self.empty_view_description).to_be_visible()
-        expect(self.empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
-
-    def check_visible_create_course_button(self):
-        expect(self.create_course_button).to_be_visible()
-
-    def click_create_course_button(self):
-        self.create_course_button.click()
-
-    def check_visible_course_card(self, index: int, title: str, max_score: str, min_score: str, estimated_time: str):
-        expect(self.course_title.nth(index)).to_be_visible()
-        expect(self.course_title.nth(index)).to_have_text(title)
-
-        expect(self.course_image.nth(index)).to_be_visible()
-
-        expect(self.course_max_score_text.nth(index)).to_be_visible()
-        expect(self.course_max_score_text.nth(index)).to_have_text(f"Max score: {max_score}")
-
-        expect(self.course_min_score_text.nth(index)).to_be_visible()
-        expect(self.course_min_score_text.nth(index)).to_have_text(f"Min score: {min_score}")
-
-        expect(self.course_estimated_time_text).to_be_visible()
-        expect(self.course_estimated_time_text).to_have_text(f"Estimated time: {estimated_time}")
-
-
-    def click_edit_course(self, index):
-        self.course_menu_button.nth(index).click()
-
-        expect(self.course_edit_menu_item.nth(index)).to_be_visible()
-        self.course_edit_menu_item.nth(index).click()
-
-    def click_delete_course(self, index):
-        self.course_menu_button.nth(index).click()
-
-        expect(self.course_delete_menu_item.nth(index)).to_be_visible()
-        self.course_delete_menu_item.nth(index).click()
-
+        self.empty_view.check_visible(
+            title="There is no results",
+            description="Results from the load test pipeline will be displayed here"
+        )
 
 
 
