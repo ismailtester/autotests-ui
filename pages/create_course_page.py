f5a5ce9b60010aefbf4e1338f7712e81a1c9ddd7
Ismail <ismailtester@yandex.ru>
2026-05-11 21:33:04 +0300

    Практикуемся в написании компонентов с использованием паттерна PageComponent

--- pages/create_course_page.py
+++ pages/create_course_page.py
@@ -1,9 +1,13 @@
 from playwright.sync_api import Page, expect
 
+from components.base_component import BaseComponent
 from components.courses.create_course_exercise_form_component import CreateCourseExerciseFormComponent
+from components.courses.create_course_exercises_toolbar_view_component import CreateCourseExercisesToolbarViewComponent
+from components.courses.create_course_toolbar_view_component import CreateCourseToolbarViewComponent
 from components.navigation.navbar_component import NavbarComponent
 from components.views.empty_view_component import EmptyViewComponent
 from components.views.image_upload_widget_component import ImageUploadWidgetComponent
+from components.courses.create_course_form_component import CreateCourseFormComponent
 from pages.base_page import BasePage
 
 
@@ -13,100 +17,16 @@ class CreateCoursePage(BasePage):
         super().__init__(page)
         # Заголовок и кнопка создания курса
         self.navbar = NavbarComponent(page)
-        self.create_course_form = CreateCourseExerciseFormComponent(page)
+        self.create_exercise_form = CreateCourseExerciseFormComponent(page)
         self.image_upload_widget = ImageUploadWidgetComponent(page, identifier='create-course-preview')
+        self.create_course_form = CreateCourseFormComponent(page)
         self.exercises_empty_view = EmptyViewComponent(page, identifier='create-course-exercises')
-
-        self.create_course_title = self.page.get_by_test_id('create-course-toolbar-title-text')
-        self.create_course_button = self.page.get_by_test_id('create-course-toolbar-create-course-button')
-
-
-        # Форма создания курса
-        self.create_course_title_input = self.page.get_by_test_id('create-course-form-title-input').locator('input')
-        self.create_course_estimated_time_input = self.page.get_by_test_id('create-course-form-estimated-time-input').locator('input')
-        self.create_course_description_textarea = page.get_by_test_id(
-            'create-course-form-description-input'
-        ).locator('textarea').first
-        self.course_max_score_input = self.page.get_by_test_id('create-course-form-max-score-input').locator('input')
-        self.course_min_score_input = self.page.get_by_test_id('create-course-form-min-score-input').locator('input')
-
-        # Заголовок и кнопка создания задания
-        self.exercises_title = self.page.get_by_test_id('create-course-exercises-box-toolbar-title-text')
-        self.create_exercise_button = self.page.get_by_test_id('create-course-exercises-box-toolbar-create-exercise-button')
+        self.create_course_toolbar = CreateCourseToolbarViewComponent(page)
+        self.create_exercise_toolbar = CreateCourseExercisesToolbarViewComponent(page)
 
 
 
 
-    def check_visibility_create_course_title(self):
-        expect(self.create_course_title).to_be_visible()
-        expect(self.create_course_title).to_have_text('Create course')
-
-    def click_create_course_button(self):
-        self.create_course_button.click()
-
-    def check_visibility_create_course_button(self):
-        expect(self.create_course_button).to_be_visible()
-
-    def check_disabled_create_course_button(self):
-        expect(self.create_course_button).to_be_disabled()
-
-
-
-    def check_visible_create_course_form(
-            self,
-            title: str,
-            estimated_time: str,
-            description: str,
-            max_score: str,
-            min_score: str
-    ):
-        expect(self.create_course_title_input).to_be_visible()
-        expect(self.create_course_title_input).to_have_value(title)
-
-        expect(self.create_course_estimated_time_input).to_be_visible()
-        expect(self.create_course_estimated_time_input).to_have_value(estimated_time)
-
-        expect(self.create_course_description_textarea).to_be_visible()
-        expect(self.create_course_description_textarea).to_have_value(description)
-
-        expect(self.course_max_score_input).to_be_visible()
-        expect(self.course_max_score_input).to_have_value(max_score)
-
-        expect(self.course_min_score_input).to_be_visible()
-        expect(self.course_min_score_input).to_have_value(min_score)
-
-    def fill_create_course_form(
-            self,
-            title: str,
-            estimated_time: str,
-            description: str,
-            max_score: str,
-            min_score: str
-    ):
-        self.create_course_title_input.fill(title)
-        expect(self.create_course_title_input).to_have_value(title)
-
-        self.create_course_estimated_time_input.fill(estimated_time)
-        expect(self.create_course_estimated_time_input).to_have_value(estimated_time)
-
-        self.create_course_description_textarea.fill(description)
-        expect(self.create_course_description_textarea).to_have_value(description)
-
-        self.course_max_score_input.fill(max_score)
-        expect(self.course_max_score_input).to_have_value(max_score)
-
-        self.course_min_score_input.fill(min_score)
-        expect(self.course_min_score_input).to_have_value(min_score)
-
-    def check_visible_exercises_title(self):
-        expect(self.exercises_title).to_be_visible()
-        expect(self.exercises_title).to_have_text('Exercises')
-
-    def check_visible_create_exercise_button(self):
-        expect(self.create_exercise_button).to_be_visible()
-
-    def click_create_exercise_button(self):
-        self.create_exercise_button.click()
 
     def check_visible_exercises_empty_view(self):
         self.exercises_empty_view.check_visible(
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -9,7 +9,7 @@ def test_successful_registration(registration_page: RegistrationPage, dashboard_
     registration_page.visit(
         'https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration'
     )
-    registration_page.fill_registration_form(email='user.name@gmail.com', username='username', password='password')
+    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
     registration_page.click_registration_button()
 
-    dashboard_page.check_dashboard_title_text()
+    dashboard_page.dashboard.check_visible()
